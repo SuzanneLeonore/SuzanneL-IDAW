612fde817e50d2a5fb0f69ecf006b6e7dc625b22
SuzanneLeonore <suzanne.girardjollet@gmail.com>
2024-10-11 10:48:18 +0200

    Création TP3, fin de la partie avec les requêtes GET

---
+++ TP3/connected.php
@@ -0,0 +1,28 @@
+<?php
+    $users = array(
+        'a moi'=>'bb',
+        'riri' => 'fifi',
+        'yoda' => 'maitrejedi' );
+
+$login = "anonymous";
+$errorText = "";
+$successfullyLogged = false;
+
+if(isset($_GET['login']) && isset($_GET['password'])) {
+    $tryLogin=$_GET['login'];
+    $tryPwd=$_GET['password'];
+
+    if( array_key_exists($tryLogin,$users) && $users[$tryLogin]==$tryPwd ) {
+        $successfullyLogged = true;
+        $login = $tryLogin;
+    } else
+        $errorText = "Erreur de login/password";
+} else
+    $errorText = "Merci d'utiliser le formulaire de login";
+
+if(!$successfullyLogged) {
+    echo $errorText;
+} else {
+    echo "<h1>Bienvenu ".$login."</h1>";
+}
+?>
